e1fb542c14879a0d8623690ecb2ee6c43cd1ca5e
Chris Whitten <chriswhitten@swagbook.domain>
2017-03-13 21:09:12 -0700

    Adds Item component

---
+++ src/components/Item.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li>
+      <div
+        :class='{bold: isFolder}'
+        @click='toggle'>
+          <div v-if='isFolder'>
+            {{ model.name }}
+            <span class='operator' v-if="isFolder">[{{open ? '-' : '+'}}]</span>
+          </div>
+          <div v-else>
+            <a v-if='hasLink' v-bind:href='model.link' target='_blank'>{{ model.name }}</a>
+            <div v-else>
+              {{ model.name }}
+            </div>
+          </div>
+      </div>
+      <ul v-show='open' v-if='isFolder'>
+        <item
+          class='item'
+          v-for='model in model.children'
+          :key='model'
+          :model='model'>
+        </item>
+      </ul>
+    </li>
+  </ul>
+</template>
